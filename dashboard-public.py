a371d7f5d4bb3eb712a957890ddc118453b19c6c
joey-van-alphen2 <120799916+joey-van-alphen2@users.noreply.github.com>
2022-12-20 19:11:17 +0100

    Update dashboard-public.py

--- dashboard-public.py
+++ dashboard-public.py
@@ -31,9 +31,10 @@ def main():
         #add_col2 = st.number_input('Verwarming', min_value=0000.00, step=0000.01, value=df1['Verwarming'].iloc[-1], format='%f')
         add_col2 = st.number_input('Verwarming', min_value=df1['Verwarming'].iloc[-1], step=0.05, value=df1['Verwarming'].iloc[-1])#, format='%f')
         add_col3 = st.number_input('Water', min_value=000.0, step=000.1, value=df1['Water'].iloc[-1], format='%f')
+        add_col4 = st.number_input('Temperatuur', step=1.0, value=df1['Temperatuur'].iloc[-1], format='%f')
         submit = st.form_submit_button('Submit')
         if submit:
-            new_data = {'Datum': add_col1, 'Verwarming': add_col2, 'Water': add_col3}
+            new_data = {'Datum': add_col1, 'Verwarming': add_col2, 'Water': add_col3, 'Temperatuur' : add_col4}
 
             df1 = df1.append(new_data, ignore_index=True)
             df1.to_csv('df1.csv', index=False)
@@ -63,22 +64,38 @@ def main():
     df_week = df1.groupby('Week')[['GJ','m3']].sum().reset_index().tail(10)
     df_month = df1.groupby('Maand')[['GJ','m3']].sum().reset_index()
     df_year = df1.groupby('Jaar')[['GJ','m3']].sum().reset_index()
+#   Gemiddelde temperatuur per maand berekenen
+    df_temp = df1.groupby('Week')['Temperatuur'].mean().to_frame().reset_index()
+    df_temp['Temperatuur'] = df_temp['Temperatuur'].round(decimals=1)
 
 #   plot voor verwarming
-    fig1 = go.Figure()
 
+#   Create figure with secondary y-axis
+    fig1 = make_subplots(specs=[[{"secondary_y": True}]])
+
+#   Add Traces
     fig1.add_trace(
         go.Bar(x=df_week_show['Dag'],
-                   y=df_week_show['GJ'], texttemplate="%{y}", width=0.5))
+                   y=df_week_show['GJ'], texttemplate="%{y}", marker={'color': 'rgb(104, 92, 148)'}, width=0.5, name='Verbruik'))
+    fig1.add_trace(
+        go.Scatter(x=df_week_show['Dag'],
+                   y=df_week_show['Temperatuur'], text=df_week_show['Temperatuur'], 
+                   name='Temperatuur', mode='lines+markers+text', textposition='top center',
+                   marker={'size': 8}), marker_color='rgb(124, 196, 139)'), secondary_y=True,)
     fig1.add_trace(
         go.Bar(x=df_week['Week'],
-                   y=df_week['GJ'], texttemplate="%{y}", width=0.5, visible=False))
+                   y=df_week['GJ'], texttemplate="%{y}", marker={'color': 'rgb(104, 92, 148)'}, width=0.5, visible=False, name='Verbruik'))
     fig1.add_trace(
-        go.Bar(x=df_month['Maand'],
-                   y=df_month['GJ'], texttemplate="%{y}", width=0.5, visible=False))
+        go.Scatter(x=df_temp['Week'],
+                   y=df_temp['Temperatuur'], text=df_temp['Temperatuur'], 
+                   name='Temperatuur', mode='lines+markers+text', textposition='top center',
+                   marker={'size': 8}, marker_color='rgb(124, 196, 139)'), visible=False), secondary_y=True,)
     fig1.add_trace(
-        go.Bar(x=df_year['Jaar'],
-                   y=df_year['GJ'], texttemplate="%{y}", width=0.5, visible=False))
+        go.Bar(x=df_month['Maand'], marker={'color': 'rgb(104, 92, 148)'},
+                   y=df_month['GJ'],visible=False, width=0.5))
+    fig1.add_trace(
+        go.Bar(x=df_year['Jaar'], marker={'color': 'rgb(104, 92, 148)'},
+                   y=df_year['GJ'],visible=False))
 
     fig1.update_layout(
         updatemenus=[
@@ -91,35 +108,37 @@ def main():
                 buttons=list([
                     dict(label="Dag",
                          method="update",
-                         args=[{"visible": [True, False, False, False]}]),
+                         args=[{"visible": [True, True, False, False, False, False]}]),
                     dict(label="Week",
                          method="update",
-                         args=[{"visible": [False, True, False, False]}]),
+                         args=[{"visible": [False, False, True, True, False, False]}]),
                     dict(label="Maand",
                          method="update",
-                         args=[{"visible": [False, False, True, False]}]),
+                         args=[{"visible": [False, False, False, False, True, False]}]),
                     dict(label="Jaar",
                          method="update",
-                         args=[{"visible": [False, False, False, True]}])
+                         args=[{"visible": [False, False, False, False, False, True]}])
 
                 ]))], dragmode='pan')  
-    
-    fig1.update_yaxes(title_text="Verbruik in GJ")
+
+    fig1.update_yaxes(showticklabels=False, showgrid=False, secondary_y=True)
+    fig1.update_yaxes(title_text="Verbruik in GJ", showticklabels=True, showgrid=True, secondary_y=False)
+    fig1.update_layout(height=500, width=800)
     
 #   Plot voor tap water
     fig2 = go.Figure()
 
     fig2.add_trace(
-        go.Bar(x=df_week_show['Dag'],
+        go.Bar(x=df_week_show['Dag'], marker={'color': 'rgb(104, 92, 148)'},
                    y=df_week_show['m3'], texttemplate="%{y}", width=0.5))
     fig2.add_trace(
-        go.Bar(x=df_week['Week'],
+        go.Bar(x=df_week['Week'], marker={'color': 'rgb(104, 92, 148)'}, 
                    y=df_week['m3'], texttemplate="%{y}", width=0.5, visible=False))
     fig2.add_trace(
-        go.Bar(x=df_month['Maand'],
+        go.Bar(x=df_month['Maand'], marker={'color': 'rgb(104, 92, 148)'}, 
                    y=df_month['m3'], texttemplate="%{y}", width=0.5, visible=False))
     fig2.add_trace(
-        go.Bar(x=df_year['Jaar'],
+        go.Bar(x=df_year['Jaar'], marker={'color': 'rgb(104, 92, 148)'},
                    y=df_year['m3'], texttemplate="%{y}", width=0.5, visible=False))
 
     fig2.update_layout(
@@ -147,6 +166,7 @@ def main():
                 ]))], dragmode='pan')  
         
     fig2.update_yaxes(title_text="Verbruik in m3")
+    fig2.update_layout(height=500, width=730)
         
 #   4 kpi's maken 
     kpi1, kpi2, kpi3, kpi4 = st.columns(4)
