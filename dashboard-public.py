675802cae71202805c8f61e289e39941399d29eb
joey-van-alphen2 <120799916+joey-van-alphen2@users.noreply.github.com>
2023-01-15 19:17:36 +0100

    Update dashboard-public.py

--- dashboard-public.py
+++ dashboard-public.py
@@ -240,11 +240,9 @@ def main():
             value=f'€ {round((df1[df1["Jaar"] == selected_year].m3.sum()*9.92), 2)}')
 
 #   Plots met kpi's weergeven    
-    st.header('Verbruik afgelopen week')
-    #st.set_dataframe_style(style="background-color:#F4D03F;")
-    #st.beta_css("table.css")
+    st.header('Meterstanden afgelopen 7 dagen')
     st.dataframe(df_week_show_st)
-    #st.table(df_week_show_st, style="background-color:#F4D03F;")
+    
     st.subheader('Verbruik stadsverwarming')
     
     kpi1, kpi2, kpi3, kpi4 = st.columns(4)
